6e85b888c486485ecaa1f3ba8d3e390f31d9336d
zhaozhentao <qq344696734@gmail.com>
2021-07-04 09:09:20 +0800

    🎨打开两个不同的flutter页面

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.purple),
       routes: {
         'index': (context) {
           return IndexRoute();
