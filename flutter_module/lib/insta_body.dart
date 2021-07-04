3a12f9b220df824496878f437ae0a07412f76eed
zhaozhentao <qq344696734@gmail.com>
2021-07-04 10:00:52 +0800

    🎨format

--- flutter_module/lib/insta_body.dart
+++ flutter_module/lib/insta_body.dart
@@ -6,10 +6,7 @@ class InstaBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        // Expanded(flex: 1, child: new InstaStories()),
-        Flexible(child: InstaList())
-      ],
+      children: <Widget>[Flexible(child: InstaList())],
     );
   }
 }
--- flutter_module/lib/insta_stories.dart
+++ flutter_module/lib/insta_stories.dart
@@ -24,35 +24,35 @@ class InstaStories extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: 5,
         itemBuilder: (context, index) => new Stack(
-              alignment: Alignment.bottomRight,
-              children: <Widget>[
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new NetworkImage(
-                            "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
-                  ),
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                index == 0
-                    ? Positioned(
-                        right: 10.0,
-                        child: new CircleAvatar(
-                          backgroundColor: Colors.blueAccent,
-                          radius: 10.0,
-                          child: new Icon(
-                            Icons.add,
-                            size: 14.0,
-                            color: Colors.white,
-                          ),
-                        ))
-                    : new Container()
-              ],
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            new Container(
+              width: 60.0,
+              height: 60.0,
+              decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                image: new DecorationImage(
+                    fit: BoxFit.fill,
+                    image: new NetworkImage(
+                        "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
             ),
+            index == 0
+                ? Positioned(
+                    right: 10.0,
+                    child: new CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      radius: 10.0,
+                      child: new Icon(
+                        Icons.add,
+                        size: 14.0,
+                        color: Colors.white,
+                      ),
+                    ))
+                : new Container()
+          ],
+        ),
       ),
     ),
   );
